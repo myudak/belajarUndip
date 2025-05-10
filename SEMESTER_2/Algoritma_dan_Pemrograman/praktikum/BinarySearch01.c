d09ffbcdeb96590a9e546055c3e86f6c0e85c716
Muchamad Yuda Tri Ananda <69108782+myudak@users.noreply.github.com>
2025-05-10 22:23:31 +0700

    praktikum d2 hackerrank

---
+++ SEMESTER_2/Algoritma_dan_Pemrograman/praktikum/BinarySearch01.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+/*
+    NAMA : Muchammad Yuda Tri Ananda
+    NIM : 24060124110142
+    KELAS : D
+    LAB : D2
+*/
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int N;
+    scanf("%d", &N);
+    int A[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+
+    int Q;
+    scanf("%d", &Q);
+
+    int X[Q];
+    int Y[Q];
+
+    for (int i = 0; i < Q; i++)
+    {
+        scanf("%d %d", &X[i], &Y[i]);
+    }
+
+    for (int i = 0; i < Q; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < N; j++)
+        {
+            if (A[j] > X[i] && A[j] <= Y[i])
+            {
+                count++;
+            }
+        }
+        printf("%d\n", count);
+    }
+
+    return 0;
+}
---
+++ SEMESTER_2/Algoritma_dan_Pemrograman/praktikum/BinarySearch02.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+/*
+    NAMA : Muchammad Yuda Tri Ananda
+    NIM : 24060124110142
+    KELAS : D
+    LAB : D2
+*/
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int N;
+    scanf("%d", &N);
+
+    int A[N];
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+
+    int Q;
+    scanf("%d", &Q);
+
+    int X[Q];
+    for (int i = 0; i < Q; i++)
+    {
+        scanf("%d", &X[i]);
+    }
+
+    for (int i = 0; i < Q; i++)
+    {
+        int jawaban = 0;
+
+        int kanan = 1;
+        for (int kiri = 0; kiri < N; kiri++)
+        {
+            if (X[i] <= A[kiri])
+            {
+                jawaban = kiri + 1;
+                break;
+            }
+            else if (X[i] - A[kiri] <= A[kanan])
+            {
+                jawaban = kanan + 1;
+                break;
+            }
+            else
+            {
+                kanan++;
+                X[i] -= A[kiri];
+            }
+        }
+        printf("%d\n", jawaban);
+    }
+
+    return 0;
+}
